bf634e41d172042635029acbcc0c51716363ac13
okmon12 <143723986+okmon12@users.noreply.github.com>
2023-10-08 14:46:31 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,6 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(content: params[:content])
-    erender json:{ post: post } 
+    render json:{ post: post }
   end
 end
